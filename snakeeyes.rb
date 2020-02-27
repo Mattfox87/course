9458ba28127d01965810fe4b01cf208ee9f0449e
Matt Ryan <mattryanjersey@gmail.com>
2020-02-27 19:41:54 +0000

    added total to output

--- snakeeyes.rb
+++ snakeeyes.rb
@@ -2,7 +2,14 @@
 # puts "dice rolled a #{dice}"
 puts "how many dice would you like to roll?"
 rolls =gets.strip.to_i
+
+dicetotal = 0 
+
 (1..rolls).each do 
   dice =rand 1..6
-puts "dice rolled a #{dice}"
+  puts "dice rolled a #{dice}"
+  dicetotal = dicetotal + dice
+  # puts "The running total is #{dicetotal}"
 end
+
+puts "The overall total was #{dicetotal}" 
