9d293e92e08f89706d5ef22a8a008d47d3c56bdb
mattfox87 <matt.l.fox1987@gmail.com>
2020-02-27 18:52:39 +0000

    working timestable

--- timestable.rb
+++ timestable.rb
@@ -1,5 +1,8 @@
-def timestable 
+def timestable(multiplier)
 (1..12).each do |number|
-  puts "#{number} x 2 = #{2*number}"
+  puts "#{number} x #{multiplier} = #{multiplier*number}"
 end
 end
+puts "select timestable"
+timestable_option =gets
+timestable(timestable_option.strip.to_i)
