6418225b3c181dd5bdebbf35ba13f747803965a5
Matt Ryan <mattryanjersey@gmail.com>
2020-02-27 19:12:02 +0000

    added method to prevent repitition

--- timestable.rb
+++ timestable.rb
@@ -2,14 +2,20 @@ def timestable(multiplier)
   if multiplier == -1
     (1..12).each do |tt|
       (1..12).each do |number|
-        puts "#{number} x #{tt} = #{number*tt}"
+        # puts "#{number} x #{tt} = #{number*tt}"
+        xxx_timestable(tt,number)
       end
     end
   else
     (1..12).each do |number|
-      puts "#{number} x #{multiplier} = #{multiplier*number}"
+      # puts "#{number} x #{multiplier} = #{multiplier*number}"
+      xxx_timestable(number,multiplier)
     end
   end
+end
+
+def xxx_timestable(number, multiplier)
+  puts "#{number} x #{multiplier} = #{multiplier*number}"
 end
 
 puts "select timestable"
