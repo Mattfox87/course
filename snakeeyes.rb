35cad5b9e4e2452e232be3edc47bdf53014a8a56
mattfox87 <matt.l.fox1987@gmail.com>
2020-02-27 19:33:18 +0000

    individual dice rolls

--- snakeeyes.rb
+++ snakeeyes.rb
@@ -1,2 +1,8 @@
-dice =rand 1..6
+# dice =rand 1..6
+# puts "dice rolled a #{dice}"
+puts "how many dice would you like to roll?"
+rolls =gets.strip.to_i
+(1..rolls).each do 
+  dice =rand 1..6
 puts "dice rolled a #{dice}"
+end
