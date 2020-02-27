20d94f6390884621bd702880f2710a3a7a1e3e97
Matt Ryan <mattryanjersey@gmail.com>
2020-02-27 19:56:02 +0000

    completetd class exercise on timestable

--- timestable.rb
+++ timestable.rb
@@ -1,25 +1,40 @@
-def timestable(multiplier)
-  if multiplier == -1
-    (1..12).each do |tt|
-      (1..12).each do |number|
-        # puts "#{number} x #{tt} = #{number*tt}"
-        xxx_timestable(tt,number)
-      end
-    end
-  else
+class Timestable_class
+  def tt(selectedTT)
     (1..12).each do |number|
-      # puts "#{number} x #{multiplier} = #{multiplier*number}"
-      xxx_timestable(number,multiplier)
+      puts "#{number} x #{selectedTT} = #{selectedTT*number}"
     end
   end
 end
 
-def xxx_timestable(number, multiplier)
-  puts "#{number} x #{multiplier} = #{multiplier*number}"
-end
+
+
+# def timestable(multiplier)
+#   if multiplier == -1
+#     (1..12).each do |tt|
+#       (1..12).each do |number|
+#         # puts "#{number} x #{tt} = #{number*tt}"
+#         xxx_timestable(tt,number)
+#       end
+#     end
+#   else
+#     (1..12).each do |number|
+#       # puts "#{number} x #{multiplier} = #{multiplier*number}"
+#       xxx_timestable(number,multiplier)
+#     end
+#   end
+# end
+
+# def xxx_timestable(number, multiplier)
+#   puts "#{number} x #{multiplier} = #{multiplier*number}"
+# end
 
 puts "select timestable"
 
-timestable_option =gets
+timestable_option = gets.strip.to_i
 
-timestable(timestable_option.strip.to_i)
+timestable_obj = Timestable_class.new
+
+timestable_obj.tt(timestable_option)
+
+
+# timestable(timestable_option)
