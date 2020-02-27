ebf90878704194968fd00822d4b218dcec687d5c
Matt Ryan <mattryanjersey@gmail.com>
2020-02-27 19:01:17 +0000

    added -1 functionality

--- timestable.rb
+++ timestable.rb
@@ -1,8 +1,19 @@
 def timestable(multiplier)
-(1..12).each do |number|
-  puts "#{number} x #{multiplier} = #{multiplier*number}"
+  if multiplier == -1
+    (1..12).each do |tt|
+      (1..12).each do |number|
+        puts "#{number} x #{tt} = #{number*tt}"
+      end
+    end
+  else
+    (1..12).each do |number|
+      puts "#{number} x #{multiplier} = #{multiplier*number}"
+    end
+  end
 end
-end
+
 puts "select timestable"
+
 timestable_option =gets
+
 timestable(timestable_option.strip.to_i)
