c479ee0aab1644be6afa444239ce331aa32aa532
Matt Ryan <mattryanjersey@gmail.com>
2020-03-05 19:29:06 +0000

    code works, moving on

--- BankAccount/bankaccount.rb
+++ BankAccount/bankaccount.rb
@@ -1,19 +1,26 @@
 class BankAccount
-  attr_accessor :account_holder, :sort_code, :account_number, :initial_balance
+  attr_accessor :account_holder, :sort_code, :account_number, :balance
 
-  def initialize(account_holder:, sort_code:, account_number:, initial_balance:)
+  def initialize(account_holder:, sort_code:, account_number:, balance:)
     @account_holder = account_holder
     @sort_code = sort_code
     @account_number = account_number
-    @initial_balance = initial_balance
+    @balance = balance
+    @interest = 1.5
   end
 
   def print_acc_holder
-    puts "The account is in the name of: #{account_holder}"
+    puts "The account is in the name of: #{@account_holder}"
   end
 
+  def deposit(amount)
+    transaction_history = {}
+    @balance += amount
+    puts "New Balance is: #{balance}"
+  end
 
 end
 
-new_account = BankAccount.new(account_holder: "Matt", sort_code: "11-22-33", account_number: "0123456789", initial_balance: 0)
+new_account = BankAccount.new(account_holder: "Matt", sort_code: "11-22-33", account_number: "0123456789", balance: 0)
 new_account.print_acc_holder
+new_account.deposit(10.00)
